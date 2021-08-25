62c3e9307323e474204d33774290031569a06c7b
Zhandor <zhandorlg@gmail.com>
2021-08-24 22:08:28 -0300

    aula 05 - projetinho calc operações basicas concluidas, interface fora de padrão

--- calculator.py
+++ calculator.py
@@ -9,7 +9,7 @@ display.grid(row = 0, column = 0, columnspan = 4, padx = 10, pady = 10)
 result = 0
 lastOperation = ''
 
-def button_click(number):    
+def button_click(number):
     newVal = str(display.get()) + str(number)    
     clear_display()
     fill_display(newVal)
@@ -23,27 +23,82 @@ def clear_click():
     global result
     result = 0
 
+def run_last_operation():
+    global lastOperation
+    
+    if(lastOperation == 'add'):
+        add_click()
+    elif(lastOperation == 'subtract'): 
+        subtract_click()
+    elif(lastOperation == 'multiply'): 
+        multiply_click()
+    elif(lastOperation == 'divide'): 
+        divide_click()
+    else:
+        print('nenhuma operação em memória')
+    lastOperation = ''
+
 def add_click():
-    global result
-    currentResult = result
-    result = int(currentResult) + int(display.get())
-    print("currentResult:", currentResult, "display value:", display.get(), "result: ", result)
-    print(currentResult, "+", display.get(), "=", result)
+    global result, lastOperation
+
+    currentVal = float(display.get())
+    if(lastOperation == ''):
+        result = currentVal
+    else:
+        currentResult = result
+        result = float(currentResult) + currentVal
+
+    lastOperation = 'add'
+    
     clear_display()
 
 def subtract_click():
-    global result
-    currentResult = result
-    result = int(currentResult) - int(display.get())
-    print("currentResult:", currentResult, "display value:", display.get(), "result: ", result)
-    print(currentResult, "-", display.get(), "=", result)
+    global result, lastOperation
+    
+    currentVal = float(display.get())
+    if(lastOperation == ''):
+        result = currentVal
+    else:
+        currentResult = result
+        result = float(currentResult) - currentVal
+
+    lastOperation = 'subtract'
+    
+    clear_display()
+
+def multiply_click():
+    global result, lastOperation
+    
+    currentVal = float(display.get())
+    if(lastOperation == ''):
+        result = currentVal
+    else:
+        currentResult = result
+        result = float(currentResult) * currentVal
+
+    lastOperation = 'multiply'
+
+    clear_display()
+
+def divide_click():
+    global result, lastOperation
+    
+    currentVal = float(display.get())
+    if(lastOperation == ''):
+        result = currentVal
+    else:
+        currentResult = result
+        result = float(currentResult) / currentVal
+
+    lastOperation = 'divide'
+    
     clear_display()
 
 def fill_display(value):
     display.insert(0, value)
 
 def equals_click():
-    add_click()
+    run_last_operation()
     global result
     fill_display(result)
     
@@ -60,9 +115,9 @@ button_9 = Button(root, text = "9", padx = 40,  pady = 20, command = lambda: but
 button_0 = Button(root, text = "0", padx = 135, pady = 20, command = lambda: button_click(0))
 
 button_add      = Button(root, text = "+",      padx = 40, pady = 20, command = lambda: add_click())
-button_subtract = Button(root, text = "-",      padx = 40, pady = 20, command = lambda: add_click())
-button_multiply = Button(root, text = "x",      padx = 40, pady = 20, command = lambda: add_click())
-button_divide   = Button(root, text = "/",      padx = 40, pady = 20, command = lambda: add_click())
+button_subtract = Button(root, text = "-",      padx = 40, pady = 20, command = lambda: subtract_click())
+button_multiply = Button(root, text = "*",      padx = 40, pady = 20, command = lambda: multiply_click())
+button_divide   = Button(root, text = "/",      padx = 40, pady = 20, command = lambda: divide_click())
 button_equals   = Button(root, text = "=",      padx = 85, pady = 20, command = lambda: equals_click())
 button_clear    = Button(root, text = "Clear",  padx = 85, pady = 20, command = lambda: clear_click())
 
@@ -82,7 +137,6 @@ button_2.grid(row = 3 , column = 1)
 button_3.grid(row = 3 , column = 2)
 button_multiply.grid(row = 3 , column = 3)
 button_divide.grid(row = 3 , column = 4)
-
 
 button_0.grid(row = 4 , column = 0, columnspan = 3)
 button_equals.grid(row = 4 , column = 3, rowspan = 2)
